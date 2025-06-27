a9b7a9210705f2c12e06edbfabcbdc2d55b1a119
Louis Fernando <ferlouis55@gmail.com>
2025-06-27 10:35:41 +0700

    Initialize a project ; Set up tailwind config ; Set up auth with GitHub Authentication using Auth.js

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+    return (
+        <main className='font-mono'>
+            <Navbar />
+            {children}
+        </main>
+    )
+}
+
+export default Layout
